bcd0e8caee48ab3511d03cc40afc6863628d67bc
justBRENDAA <brendaa.scholar@gmail.com>
2024-02-27 17:56:40 -0600

    create display menu function

--- PseudoBankingSystem/PBS.cpp
+++ PseudoBankingSystem/PBS.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 using namespace std;
 
-
+void displayMenu(int&);
 
 
 int main() {
-	// test commit and push
-	int num = 1
+	// CONSTANTS FOR MENU CHOICES
+	const int ACC_SUMMARY_CHOICE = 1,
+			  DEPOSIT_CHOICE = 2,
+			  WITHDRAW_CHOICE = 3,
+			  QUIT_CHOICE = 4;
+	int choice;
+	 
+	// DISPLAY MENU AND OBTAIN USER CHOICE
+	displayMenu(choice);
+	cout << "in main" << choice;
 
 	return 0;
+}
+
+void displayMenu(int &choice) {
+	cout << "\n\tOnline Banking Menu\n"
+		 << "--------------------------\n"
+		 << "1. Display Account Summary\n"
+		 << "2. Deposit into Account\n"
+		 << "3. Withdraw from Account\n"
+		 << "4. Quit the Program\n\n"
+		 << "Enter your choice: ";
+
+	cin >> choice;
+
+	while (choice < 0 || choice > 4) {
+		cout << "\nChoice must be between 1-4. Please try again.\n"
+			 << "Enter your choice: ";
+
+		cin >> choice;
+	}
 }
